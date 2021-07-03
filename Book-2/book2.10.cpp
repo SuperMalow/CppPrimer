3b08d8faf2c4ad308bd384205d94b675d52bfcfa
Supermalow <229538252@qq.com>
2021-07-03 21:02:12 +0800

    master

---
+++ Book-2/book2.10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string global_str;
+int global_int;
+int main()
+{
+    int local_int;
+    string local_str;
+
+    cout << global_str << endl;
+    cout << global_int << endl;
+    cout << local_str << endl;
+    cout << local_int << endl;
+    
+    system("pause");
+    return 0;
+}
