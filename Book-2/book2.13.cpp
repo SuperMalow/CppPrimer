e0d6851e13094fbdacaa2ee2ba879d6f985002db
Supermalow <229538252@qq.com>
2021-07-03 21:05:48 +0800

    master

---
+++ Book-2/book2.13.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int i = 42;
+
+int main()
+{
+    int i = 100;
+    int j = i;
+
+    cout << " j = " << j << " i = " << i << endl;
+
+    system("pause");
+    return 0;
+}
